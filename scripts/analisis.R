f22640a4ddde80588f1bd62297ef1ee68826bb48
LauraR2177 <laurapaola.rica2177@comunidadunir.net>
2026-04-21 21:03:53 -0500

    Enhance analysis with histogram and average calculation
    Added histogram creation and analysis for 'Site' column.

--- scripts/analisis.R
+++ scripts/analisis.R
@@ -1,3 +1,5 @@
+# ---  Análisis: Grafico de barra ---
+
 # Cargar la librería para leer Excel
 install.packages("readxl")
 library(readxl)
@@ -14,8 +16,41 @@ barplot(as.matrix(datos[1:5, 2:4]), col="orange")
 # Crear el archivo de imagen
 png("mi_grafico.png", width = 800, height = 600)
 
-# 2. Dibujar el gráfico 
+# Dibujar el gráfico 
 barplot(as.matrix(datos_expresion[1:5, 2:4]), main="Expresión de prueba", col="orange")
 
-# 3. Cerrar y guardar el archivo
+# Cerrar y guardar el archivo
 dev.off()
+
+# --- Histograma: Distribución de Frecuencias ---
+
+# Crear un histograma de la columna Site
+
+# Usamos log10 para que los números tan grandes sean fáciles de ver
+hist(log10(datos_expresion$Site), 
+     main="Distribución de Conteos (Escala Log)", 
+     xlab="Log10(Site)", 
+     col="salmon", 
+     border="white")
+
+# Esto nos dice el promedio exacto de esa columna
+promedio_site <- mean(datos_expresion$Site, na.rm = TRUE)
+print(paste("El promedio de la columna Site es:", promedio_site))
+
+# Indicamos a R que cree un archivo PNG en tu carpeta de trabajo
+png("histograma_expresion.png", width = 800, height = 600)
+
+# Dibujamos el gráfico (con el truco del logaritmo para que se vea bien)
+hist(log10(datos_expresion$Site + 1), 
+     main="Distribución de Conteos del Estudio GSE245700", 
+     xlab="Nivel de Expresión (Escala Log10)", 
+     col="salmon", 
+     border="white")
+
+#  Cerrar y guardar el archivo
+dev.off()
+
+# Nota: El eje horizontal (X): Representa el "Nivel de Expresión" (o el valor de los conteos).
+El eje vertical (Y): Representa la "Frecuencia" (cuántos genes o sitios tienen ese valor).
+Las barras: Si una barra es muy alta, significa que muchos genes tienen ese nivel de expresión.
+
